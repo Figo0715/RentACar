82dcf30fedb42f358bd9f9a5c05ad0764d221d90
Ömer Çetinkaya <omercetinkaya07@hotmail.com>
2021-12-19 01:43:37 +0300

    19.12.2021

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -10,5 +10,8 @@ namespace Business.Abstract
    public interface ICarService
     {
         List<Car> GetAll();
+        List<Car> GetAllByBranId(int id);
+        List<Car> GetByDailyPrice(decimal min, decimal max);
+        
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db ile proje classlarını bağlar.
+   public class RentACarContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-IM71LJG\SQLEXPRESS;Database=RentACar;Trusted_Connection=True");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+    }
+}
